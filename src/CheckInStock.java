c43003b75b52b4c24f91cd5720e8a8883b660ad9
kellarwindland <windland.6@buckeyemail.osu.edu>
2021-04-01 14:47:21 -0400

    Added micro center and working of email

--- src/CheckInStock.java
+++ src/CheckInStock.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.*;
@@ -9,25 +8,69 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import javax.print.Doc;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class CheckInStock {
 
     private Map<String, Document> docs;
     private FileWriter myWriter;
 
+    private String address = "windland.6@buckeyemail.osu.edu";
+    private String host = "smtp.example.com";
+
     public static void main(String[] args) throws IOException {
 
         CheckInStock checkInStock = new CheckInStock();
         checkInStock.checkStock();
         checkInStock.close();
+        checkInStock.sendEmail();
+    }
+
+    private void sendEmail(){
+
+            // Get system properties
+            Properties properties = System.getProperties();
+
+            // Setup mail server
+            properties.setProperty("mail.smtp.host", host);
+
+            // Get the default Session object.
+            Session session = Session.getDefaultInstance(properties);
+
+            try {
+                // Create a default MimeMessage object.
+                MimeMessage message = new MimeMessage(session);
+
+                // Set From: header field of the header.
+                message.setFrom(new InternetAddress(address));
+
+                // Set To: header field of the header.
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(address));
+
+                // Set Subject: header field
+                message.setSubject("This is the Subject Line!");
+
+                // Now set the actual message
+                message.setText("This is actual message");
+
+                // Send message
+                Transport.send(message);
+                System.out.println("Sent message successfully....");
+            } catch (MessagingException mex) {
+                mex.printStackTrace();
+            }
     }
 
     private void close() throws IOException {
         myWriter.close();
     }
 
-    public CheckInStock() throws IOException {
+    private CheckInStock() throws IOException {
         docs = new HashMap<>();
 
         File newFile = new File("data/websites.txt");
@@ -40,17 +83,48 @@ public class CheckInStock {
         myWriter = new FileWriter("F:\\CheckInStock\\data\\result.txt");
     }
 
-    public void checkStock() throws IOException {
+    private void checkStock() throws IOException {
 
         for(Map.Entry<String,Document> entry : docs.entrySet()){
-            if(entry.getKey().equals("NewEgg")){
+            System.out.println(entry.getKey());
+            myWriter.write(entry.getKey() + "\n");
+            System.out.println();
+            myWriter.write("\n");
+            if(entry.getKey().startsWith("NewEgg")){
                 checkNewEgg(entry.getValue());
+            }else if(entry.getKey().startsWith("MicroCenter")) {
+                checkMicro(entry.getValue());
+            }
+        }
+
+    }
+
+    public void checkMicro(Document doc) throws IOException {
+        Elements sections = doc.select("article#productGrid").select("ul");
+
+        for(Element element : sections) {
+            Elements elements = element.select("div.result_right").select("div.details").select("div.detail_wrapper");
+            for(Element item : elements){
+
+                System.out.println(item.select("div.pDescription.compressedNormal2").text());
+                myWriter.write(item.select("div.pDescription.compressedNormal2").text() + "\n");
+
+                if(item.select("div.stock").text().startsWith("Usually ships in")){
+                    System.out.println("ORDER NOW");
+                    myWriter.write("ORDER NOW" + "\n");
+                }else{
+                    System.out.println("OUT OF STOCK");
+                    myWriter.write("OUT OF STOCK" + "\n");
+                }
+                System.out.println();
+                myWriter.write("\n");
             }
         }
 
     }
 
     public void checkNewEgg(Document doc) throws IOException {
+
         Elements sections = doc.select("div.item-cells-wrap.border-cells.items-grid-view.four-cells.expulsion-one-cell");
 
         for(Element element : sections){
@@ -61,8 +135,16 @@ public class CheckInStock {
 
                     System.out.println(item.select("a.item-title").text());
                     myWriter.write(item.select("a.item-title").text() + "\n");
-                    System.out.println(item.select("p.item-promo").text());
-                    myWriter.write(item.select("p.item-promo").text() + "\n");
+
+                    Elements promo = item.select("p.item-promo");
+
+                    if(!promo.isEmpty()){
+                        System.out.println(item.select("p.item-promo").text());
+                        myWriter.write(item.select("p.item-promo").text() + "\n");
+                    }else{
+                        System.out.println("ORDER NOW");
+                        myWriter.write("ORDER NOW");
+                    }
                     System.out.println();
                     myWriter.write("\n");
                 }
